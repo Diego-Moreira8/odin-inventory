38d8d6629cb2170102a97ab74048c1f57d2d8bae
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-12-02 19:55:04 -0300

    feat: add express validator

--- controllers/developer.js
+++ controllers/developer.js
@@ -1,7 +1,18 @@
+const { body, validationResult } = require("express-validator");
+
 const db = require("../db/queries");
 
 const layoutView = "layouts/layout";
 const viewsDirectory = "../pages/developer";
+
+const validateForm = [
+  body("developer")
+    .trim()
+    .notEmpty()
+    .withMessage("O desenvolvedor precisa ter um nome.")
+    .isLength({ max: 50 })
+    .withMessage("O nome do desenvolvedor pode ter no máximo 50 caracteres."),
+];
 
 async function detailsGet(req, res, next) {
   const developer = await db.getDeveloper(req.params.id);
@@ -15,31 +26,51 @@ async function detailsGet(req, res, next) {
 
 async function createGet(req, res, next) {
   res.render(layoutView, {
-    partial: `${viewsDirectory}/create`,
+    partial: `${viewsDirectory}/form`,
     title: "Criar Desenvolvedor",
+    isEdit: false,
     developer: {},
+    errors: [],
   });
 }
 
-async function createPost(req, res, next) {
-  await db.createDeveloper(req.body.developer);
-  res.send(req.body.developer);
-}
+const createPost = [
+  validateForm,
+
+  async function (req, res, next) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render(layoutView, {
+        partial: `${viewsDirectory}/form`,
+        title: "Criar Desenvolvedor",
+        isEdit: false,
+        developer: { name: req.body.developer },
+        errors: errors.array(),
+      });
+    }
+
+    await db.createDeveloper(req.body.developer);
+    res.send(req.body.developer);
+  },
+];
 
 async function updateGet(req, res, next) {
   const developer = await db.getDeveloper(req.params.id);
 
   res.render(layoutView, {
-    partial: `${viewsDirectory}/create`,
+    partial: `${viewsDirectory}/form`,
     title: `Editar Desenvolvedor: ${developer.name}`,
+    errors: [],
+    isEdit: true,
     developer,
   });
 }
 
 async function updatePost(req, res, next) {
   console.log(req.body.name);
-  await db.updateDeveloper(req.params.id, req.body.developer);
-  res.redirect(`/desenvolvedor/${req.params.id}`);
+  await db.updateDeveloper(req.body.id, req.body.developer);
+  res.redirect(`/desenvolvedor/${req.body.id}`);
 }
 
 module.exports = { detailsGet, createGet, createPost, updateGet, updatePost };
--- routes/developer.js
+++ routes/developer.js
@@ -5,8 +5,8 @@ const developerRouter = Router();
 
 developerRouter.get("/criar", developerController.createGet);
 developerRouter.post("/criar", developerController.createPost);
-developerRouter.get("/editar/:id", developerController.updateGet);
-developerRouter.post("/editar/:id", developerController.updatePost);
+developerRouter.get("/:id/editar", developerController.updateGet);
+developerRouter.post("/:id/editar", developerController.updatePost);
 developerRouter.get("/:id", developerController.detailsGet);
 
 module.exports = developerRouter;
