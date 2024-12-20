d3e8b83d31b3b4f3b9807640c6a119be2f73aa91
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-12-20 20:37:46 -0300

    docs: create readme

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
-# To-dos:
+# App de Inventário
 
-- Titles
+Este é um projeto de um sistema de inventário fictício proposto pelo site [The Odin Project](https://www.theodinproject.com/) para fixar o aprendizado sobre PostgreSQL.
+
+![Captura de tela do projeto](./public/images/project-screenshot.png)
+
+## Tecnologias Utilizadas
+
+- Node.js
+- Express.js
+- PostgreSQL
+- EJS
+- Bootstrap
+
+## Como Executar
+
+1. **Clone o repositório:**
+
+```bash
+git clone https://github.com/Diego-Moreira8/odin-inventory.git
+```
+
+2. **Instale as dependências:**
+
+```bash
+cd odin-inventory
+npm install
+```
+
+3. **Configure o banco de dados:**
+
+- Certifique-se que o PostgreSQL está instalado e em execução.
+  - Caso precise de ajuda para instalar, [esta lição](https://www.theodinproject.com/lessons/nodejs-installing-postgresql#installing-postgresql) pode ser de grande ajuda.
+- Crie o banco de dados.
+  - Confira [esta lição](https://www.theodinproject.com/lessons/nodejs-using-postgresql#setting-up-the-db) caso precise de ajuda sobre como criar um banco de dados.
+- Crie um arquivo .env na raiz do projeto com o URI de conexão da base de dados. Confira o arquivo .env.example se precisar de um exemplo.
+- Execute o script para criação das tabelas:
+
+```bash
+node db/createTables.js
+```
+
+- **(Opcional)** Execute o script para popular as tabelas:
+
+```bash
+node db/populate/createTables.js
+```
+
+Esse script limpa as tabelas e insere alguns dados de exemplo.
+
+4. **Execute o aplicativo:**
+
+```bash
+npm run dev
+```
+
+Isso iniciará um servidor de desenvolvimento local na porta 3000.
+
+---
+
+_Nota: Este projeto é apenas para fins educacionais e de aprendizado. Não é recomendado para uso em produção sem considerar práticas de segurança adequadas._
--- db/populate.js
+++ db/populate.js
@@ -1,5 +1,6 @@
 const pool = require("./pool");
 const db = require("./allQueries");
+const { createTables } = require("./createTables");
 
 const genres = [];
 const developers = [];
@@ -7,79 +8,6 @@ const games = [];
 const gamesGenres = [];
 const platforms = [];
 const products = [];
-
-async function createTables() {
-  console.log("Started creating tables");
-
-  await pool.query(
-    `
-      CREATE TABLE IF NOT EXISTS genres (
-        id SERIAL PRIMARY KEY,
-        name TEXT NOT NULL UNIQUE
-      );
-    `
-  );
-
-  await pool.query(
-    `
-      CREATE TABLE IF NOT EXISTS developers (
-        id SERIAL PRIMARY KEY,
-        name TEXT NOT NULL UNIQUE
-      );
-    `
-  );
-
-  await pool.query(
-    `
-      CREATE TABLE IF NOT EXISTS games (
-        id SERIAL PRIMARY KEY,
-        title TEXT NOT NULL UNIQUE,
-        description TEXT,
-        website TEXT,
-        developer_id INT NOT NULL,
-        FOREIGN KEY (developer_id) REFERENCES developers(id)
-      );
-    `
-  );
-
-  await pool.query(
-    `
-      CREATE TABLE IF NOT EXISTS games_genres (
-        game_id INT NOT NULL,
-        genre_id INT NOT NULL,
-        PRIMARY KEY (game_id, genre_id),
-        FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-        FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
-      );
-    `
-  );
-
-  await pool.query(
-    `
-      CREATE TABLE IF NOT EXISTS platforms (
-        id SERIAL PRIMARY KEY,
-        name TEXT NOT NULL UNIQUE
-      );
-    `
-  );
-
-  await pool.query(
-    `
-      CREATE TABLE IF NOT EXISTS products (
-        id SERIAL,
-        game_id INT NOT NULL,
-        platform_id INT NOT NULL,
-        PRIMARY KEY (game_id, platform_id),
-        launch_date DATE NOT NULL,
-        price NUMERIC(9, 2) NOT NULL,
-        FOREIGN KEY (game_id) REFERENCES games(id),
-        FOREIGN KEY (platform_id) REFERENCES platforms(id)
-      );
-    `
-  );
-
-  console.log("Done creating tables");
-}
 
 async function dropTables() {
   console.log("Start dropping all tables");
